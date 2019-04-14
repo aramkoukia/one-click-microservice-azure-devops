de9b78734a8f7fec8b8622f51dabe0921e21688d
aramkoukia <aramkoukia@gmail.com>
2019-04-14 11:44:07 -0700

    added steps

---
+++ one-click-microservice/one-click-microservice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace one_click_microservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Authenticate with Azure DevOps Project");
+            Console.WriteLine(
+                "Collect Required Informaction: " +
+                "DevOps ProjectName, " +
+                "Repository Name, " +
+                "Template Project Language/Type" +
+                "Build Configuration (Kubernetes information?)" +
+                "Release Configuation (Kubernetes information?)");
+
+            Console.WriteLine("Create Azure DevOps Project");
+            Console.WriteLine("Create DevOps Repo");
+            Console.WriteLine("Create Template Project");
+            Console.WriteLine("Commit Template Project to Azure DevOps Repo");
+            Console.WriteLine("Create DevOps Release Pipeline");
+            Console.WriteLine("Create DevOps Build Pipeline");
+        }
+    }
+}
