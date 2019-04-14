ca5f85da5d46df8637298887e0b59993b1cefddb
aramkoukia <aramkoukia@gmail.com>
2019-04-14 11:51:01 -0700

    adde placeholders

--- one-click-microservice/one-click-microservice/Program.cs
+++ one-click-microservice/one-click-microservice/Program.cs
@@ -6,7 +6,31 @@ namespace one_click_microservice
     {
         static void Main(string[] args)
         {
+            AuthenticateWithAzureDevOps();
+
+            CollectServiceInformation();
+
+            CreateAzureDevOpsProject();
+
+            CreateAzureDevOpsRepository();
+
+            CreateMicroserviceServiceTemplate();
+
+            CommitTemplateProject();
+
+            CreateReleasePipeline();
+
+            CreateBuildPipeline();
+        }
+
+        private static void AuthenticateWithAzureDevOps()
+        {
+            Console.WriteLine("Enter Azure DevOps credentials:");
             Console.WriteLine("Authenticate with Azure DevOps Project");
+        }
+
+        private static void CollectServiceInformation()
+        {
             Console.WriteLine(
                 "Collect Required Informaction: " +
                 "DevOps ProjectName, " +
@@ -14,13 +38,36 @@ namespace one_click_microservice
                 "Template Project Language/Type" +
                 "Build Configuration (Kubernetes information?)" +
                 "Release Configuation (Kubernetes information?)");
+        }
 
+        private static void CreateAzureDevOpsProject()
+        {
             Console.WriteLine("Create Azure DevOps Project");
-            Console.WriteLine("Create DevOps Repo");
-            Console.WriteLine("Create Template Project");
+        }
+
+        private static void CreateAzureDevOpsRepository()
+        {
+            Console.WriteLine("Create Azure DevOps Repository");
+        }
+
+        private static void CreateMicroserviceServiceTemplate()
+        {
+            Console.WriteLine("Create Microservice Template Project");
+        }
+
+        private static void CommitTemplateProject()
+        {
             Console.WriteLine("Commit Template Project to Azure DevOps Repo");
-            Console.WriteLine("Create DevOps Release Pipeline");
-            Console.WriteLine("Create DevOps Build Pipeline");
+        }
+
+        private static void CreateBuildPipeline()
+        {
+            Console.WriteLine("Create Azure DevOps Build Pipeline");
+        }
+
+        private static void CreateReleasePipeline()
+        {
+            Console.WriteLine("Create Azure DevOps Release Pipeline");
         }
     }
 }
